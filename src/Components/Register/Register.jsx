3fafddeabe41efaab5037826f82e9613f9a9d936
Your Name <your-email@example.com>
2026-07-22 00:04:14 +0600

    create reuseable context api and using register form or section

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,31 +1,42 @@
+import { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../../Contexts/AuthContext";
 // import { auth } from "../../firebase.init";
 // import { createUserWithEmailAndPassword } from "firebase/auth";
 
-    
-
-    const handleRegisterForm = e => {
-      e.preventDefault()
-      const name = e.target.name.value;
-      const email = e.target.email.value;
-      const password = e.target.password.value;
-      // console.log(userInfo);
-
-      console.log(name);
-
-      // create a users
-      // createUserWithEmailAndPassword(auth, email, password)
-      //   .then((result) =>{
-      //     console.log(result);
-      //   })
-      //   .catch(error => {
-      //     console.log(error);
-      //   })
-
-    }
-
-
 export default function Register() {
+
+  
+  const {createUser} = use(AuthContext);
+  
+
+  const handleRegisterForm = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    // console.log(userInfo);
+
+    console.log(name);
+
+    // create a users
+    // createUserWithEmailAndPassword(auth, email, password)
+    //   .then((result) =>{
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   })
+
+        createUser(email, password)
+          .then((result) => {
+            console.log(result);
+          })
+          .catch(error =>{
+            console.log(error);
+          })
+  };
+
   return (
     <div className="card bg-base-100 w-full mx-auto  max-w-sm shrink-0 shadow-2xl mt-10 p-10">
       <h1 className="text-4xl font-bold ">Register now!</h1>
@@ -34,9 +45,19 @@ export default function Register() {
           <label className="label">Name</label>
           <input type="text" name="name" className="input" placeholder="Name" />
           <label className="label">Email</label>
-          <input type="email" name="email" className="input" placeholder="Email" />
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
           <label className="label">Password</label>
-          <input type="password" name="password" className="input" placeholder="Password" />
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
           <button className="btn btn-neutral mt-4">Register</button>
         </form>
         <p>
--- src/Contexts/AuthProvider.jsx
+++ src/Contexts/AuthProvider.jsx
@@ -1,18 +1,22 @@
+import { auth } from "../firebase.init";
 import { AuthContext } from "./AuthContext";
 import {  createUserWithEmailAndPassword } from "firebase/auth";
 
-export default function AuthProvider({children}) {
+export default function AuthProvider({ children }) {
 
-    const createUser = () => {
-        return createUserWithEmailAndPassword(auth)
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
+    const userInfo = {
+      createUser
     }
 
   return (
-    <AuthContext >
-      {
-        children
-      }
-    </AuthContext>
+    <AuthContext.Provider value={userInfo}>
+      {children}
+    </AuthContext.Provider>
   )
 }
 
